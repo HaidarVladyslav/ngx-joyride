7a2b333a15bd0acd38b50b6f6ca41bf5d99d1fa9
HaidarVladyslav <vladyslavhaidar@gmail.com>
2025-08-02 18:27:24 +0300

    ng18

--- projects/demo/src/app/components/about.component.ts
+++ projects/demo/src/app/components/about.component.ts
@@ -3,8 +3,8 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: 'about-page',
-    template: `<div joyrideStep="step1" (next)="onNext()" (prev)="onPrev()"><h1>About title</h1></div>
-               <div joyrideStep="step2" (next)="onNext()" (prev)="onPrev()">About Subtitle</div>`
+    template: `<div joyrideStep="step1" title="STEP 1 ABOUT" (next)="onNext()" (prev)="onPrev()"><h1>About title</h1></div>
+               <div joyrideStep="step2" title="STEP 2 ABOUT END" (next)="onNext()" (prev)="onPrev()">About Subtitle</div>`
 })
 export class AboutComponent {
 
